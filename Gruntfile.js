eb29a38067f03235e8140de32f8392d294352d39
Sebastiaan Deckers <cbas@pandion.be>
2013-02-18 01:14:06 +0800

    Implemented nodeunit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,23 +28,27 @@ module.exports = function(grunt) {
       tests: ['tmp'],
     },
 
+    // Work on a copy of the files because they will be renamed during testing.
+    copy: {
+      test: {
+        flatten: true,
+        expand: true,
+        src: ['test/fixtures/*.txt'],
+        dest: 'tmp/',
+      },
+    },
+
     // Configuration to be run (and then tested).
     rev: {
       default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+        src: ['tmp/default.txt']
       },
       custom_options: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!',
+          algorithm: 'sha1',
+          length: 4
         },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+        src: ['tmp/custom.txt']
       },
     },
 
@@ -59,13 +63,14 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'rev', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'copy', 'rev', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- test/rev_test.js
+++ test/rev_test.js
@@ -30,18 +30,16 @@ exports.rev = {
   default_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var exists = grunt.file.exists('tmp/9e107d9d.default.txt');
+    test.ok(exists, '8 character MD5 hash prefix');
 
     test.done();
   },
   custom_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    var exists = grunt.file.exists('tmp/2fd4.custom.txt');
+    test.ok(exists, '4 character SHA-1 hash prefix');
 
     test.done();
   },
